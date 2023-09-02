ea65a79b27c618442416204579f2ee47b84b4213
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-03 00:54:52 +0800

    added theme feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ContactForm from "./components/ContactForm";
 
 const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => (props.$theme ? "white" : "black")};
 `;
+
 const App = () => {
+  const Hour = new Date().getHours();
+  const isLight = Hour < 18 && Hour >= 6;
+  const [theme, setTheme] = useState(isLight);
+
   return (
-    <AppContainer>
-      <Header />
-      <Footer />
+    <AppContainer $theme={theme}>
+      <Header theme={theme} setTheme={setTheme} />
+      <ContactForm theme={theme} />
+      <Footer theme={theme} />
     </AppContainer>
   );
 };

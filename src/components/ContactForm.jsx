9295aea227cfd771a77c862b2af0a5c65511a325
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-03 03:33:20 +0800

    updated contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const StyledForm = styled.form`
   width: 50vw;
   min-height: 50vh;
-  padding: 2rem;
+  padding: 3rem 2rem;
   border: 5px ridge var(--focus1);
   display: flex;
   flex-direction: column;
@@ -19,25 +19,32 @@ const StyledP = styled.p`
   //doesn't look very good
 `;
 const StyledLabel = styled.label`
-  width: 50%;
+  width: 80%;
   text-align: left;
   color: var(--focus1);
   font-family: "Black Ops One", cursive;
 `;
 const StyledInput = styled.input`
-  width: 50%;
+  width: 80%;
   height: 2rem;
   border: 2px solid var(--focus1);
   margin-bottom: 1rem;
+  &:focus {
+    outline: 2px solid var(--focus1);
+  }
 `;
 const StyledTextarea = styled.textarea`
-  width: 50%;
+  width: 80%;
   min-height: 5rem;
   border: 2px solid var(--focus1);
   margin-bottom: 1rem;
+  resize: none;
+  &:focus {
+    outline: 2px solid var(--focus1);
+  }
 `;
 const StyledButton = styled.button`
-  width: 50%;
+  width: 80%;
   height: 2rem;
   font-family: "Black Ops One", cursive;
   font-size: 1rem;
@@ -45,6 +52,10 @@ const StyledButton = styled.button`
   border: none;
   border-radius: 5px;
   background-color: var(--focus2);
+  cursor: pointer;
+  &:hover {
+    background-color: white;
+  }
 `;
 
 const ContactForm = () => {
@@ -63,12 +74,12 @@ const ContactForm = () => {
       method="post"
     >
       <StyledLabel htmlFor="name">Full Name</StyledLabel>
-      <StyledInput id="name" type="text" name="name" />
+      <StyledInput id="name" type="text" name="name" required />
       <StyledLabel htmlFor="email">Email Address</StyledLabel>
-      <StyledInput id="email" type="email" name="email" />
+      <StyledInput id="email" type="email" name="email" required />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <StyledLabel htmlFor="message">Message</StyledLabel>
-      <StyledTextarea id="message" name="message" />
+      <StyledTextarea id="message" name="message" required />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <StyledButton type="submit" disabled={state.submitting}>
         Submit

c9691ab538d88016358a3df2b52d66d6b81ca190
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-03 00:54:22 +0800

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,41 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faCloud } from "@fortawesome/free-solid-svg-icons";
 
 const StyledHeader = styled.header``;
-const Header = () => {
-  return <StyledHeader></StyledHeader>;
+const Time = styled.p`
+  color: var(--focus1);
+`;
+const Icon = styled(FontAwesomeIcon)`
+  width: 1.5rem;
+  height: 1.5rem;
+  color: var(--focus1);
+`;
+const Header = ({ theme, setTheme }) => {
+  const [current, setCurrent] = useState(
+    new Date().toLocaleString("en-US", { hour12: false })
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const updatedCurrent = new Date().toLocaleString("en-US", {
+        hour12: false,
+      });
+      setCurrent(updatedCurrent);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTheme = () => setTheme((prev) => !prev);
+  return (
+    <StyledHeader>
+      <Time>{current}</Time>
+      <Icon icon={theme ? faMoon : faCloud} onClick={updateTheme} />
+    </StyledHeader>
+  );
 };
 
 export default Header;

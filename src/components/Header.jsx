cb9bb9cf62cd1de2951ba3bf6178e08189998b7c
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-03 03:34:02 +0800

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import { faCloud } from "@fortawesome/free-solid-svg-icons";
 
-const StyledHeader = styled.header``;
-const Time = styled.p`
-  color: var(--focus1);
+const StyledHeader = styled.header`
+  width: 100vw;
+  padding: 2rem;
+  position: sticky;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
 `;
-const Icon = styled(FontAwesomeIcon)`
-  width: 1.5rem;
-  height: 1.5rem;
+const Title = styled.h1`
+  font-family: "Black Ops One", cursive;
+  font-size: 2rem;
+`;
+const StyledNav = styled.nav``;
+const StyledList = styled.ul`
+  display: flex;
+  gap: 2rem;
+`;
+const StyledListItem = styled.li`
+  list-style: none;
+  font-family: "Black Ops One", cursive;
+  font-size: 1.1rem;
+`;
+const Time = styled.p`
+  width: 15rem;
+  text-align: center;
   color: var(--focus1);
+  font-family: "Lobster", cursive;
+  letter-spacing: 2px;
+`;
+const ThemeSwitch = styled.p`
+  color: var(--focus1);
+  font-size: 1.5rem;
 `;
 const Header = ({ theme, setTheme }) => {
+  const options = {
+    weekday: "short",
+    year: "2-digit",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
   const [current, setCurrent] = useState(
-    new Date().toLocaleString("en-US", { hour12: false })
+    new Date().toLocaleString("en-US", options)
   );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const updatedCurrent = new Date().toLocaleString("en-US", {
-        hour12: false,
-      });
+      const updatedCurrent = new Date().toLocaleString("en-US", options);
       setCurrent(updatedCurrent);
     }, 1000);
     return () => clearInterval(interval);
@@ -32,8 +63,20 @@ const Header = ({ theme, setTheme }) => {
   const updateTheme = () => setTheme((prev) => !prev);
   return (
     <StyledHeader>
+      <Title>Zun Liang ⊹˚₊⊹˚ʚ☕️੭</Title>
+      <StyledNav>
+        <StyledList>
+          <StyledListItem>Home</StyledListItem>
+          <StyledListItem>About Me</StyledListItem>
+          <StyledListItem>Projects</StyledListItem>
+          <StyledListItem>Blog</StyledListItem>
+          <StyledListItem>Contact</StyledListItem>
+        </StyledList>
+      </StyledNav>
       <Time>{current}</Time>
-      <Icon icon={theme ? faMoon : faCloud} onClick={updateTheme} />
+      <ThemeSwitch onClick={updateTheme}>
+        {theme ? "✩·͙*̩̩͙˚̩̥̩̥*̩̩͙✩·͙˚̩̥̩̥." : "⋆⁺₊⋆ ☀︎ ⋆⁺₊⋆"}
+      </ThemeSwitch>
     </StyledHeader>
   );
 };

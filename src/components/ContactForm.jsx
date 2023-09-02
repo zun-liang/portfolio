56283ae884a75699fddc9160af3237e338277b5c
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-03 00:51:51 +0800

    added contact form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,80 @@
+import { useForm, ValidationError } from "@formspree/react";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  width: 50vw;
+  min-height: 50vh;
+  padding: 2rem;
+  border: 5px ridge var(--focus1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+`;
+const StyledP = styled.p`
+  color: var(--focus1);
+  font-family: "Black Ops One", cursive;
+  font-size: 1.5rem;
+  //doesn't look very good
+`;
+const StyledLabel = styled.label`
+  width: 50%;
+  text-align: left;
+  color: var(--focus1);
+  font-family: "Black Ops One", cursive;
+`;
+const StyledInput = styled.input`
+  width: 50%;
+  height: 2rem;
+  border: 2px solid var(--focus1);
+  margin-bottom: 1rem;
+`;
+const StyledTextarea = styled.textarea`
+  width: 50%;
+  min-height: 5rem;
+  border: 2px solid var(--focus1);
+  margin-bottom: 1rem;
+`;
+const StyledButton = styled.button`
+  width: 50%;
+  height: 2rem;
+  font-family: "Black Ops One", cursive;
+  font-size: 1rem;
+  color: var(--focus1);
+  border: none;
+  border-radius: 5px;
+  background-color: var(--focus2);
+`;
+
+const ContactForm = () => {
+  const [state, handleSubmit] = useForm("mbjvygnp");
+  if (state.succeeded) {
+    return (
+      <StyledP>
+        Thanks for reaching out! I will get back to you as soon as possible!
+      </StyledP>
+    );
+  }
+  return (
+    <StyledForm
+      onSubmit={handleSubmit}
+      action="https://formspree.io/f/mbjvygnp"
+      method="post"
+    >
+      <StyledLabel htmlFor="name">Full Name</StyledLabel>
+      <StyledInput id="name" type="text" name="name" />
+      <StyledLabel htmlFor="email">Email Address</StyledLabel>
+      <StyledInput id="email" type="email" name="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <StyledLabel htmlFor="message">Message</StyledLabel>
+      <StyledTextarea id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <StyledButton type="submit" disabled={state.submitting}>
+        Submit
+      </StyledButton>
+    </StyledForm>
+  );
+};
+
+export default ContactForm;

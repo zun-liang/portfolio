536be6b89cf8074de5e07635711a853c0fe85383
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-10-29 18:37:54 +0800

    update readme

--- src/firebase.js
+++ src/firebase.js
@@ -22,3 +22,4 @@ export const db = getFirestore(app);
 export const blogsCollection = collection(db, "blogs");
 export const projectsCollection = collection(db, "projects");
 export const auth = getAuth();
+//how to use analytics here?
